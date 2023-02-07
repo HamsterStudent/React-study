0cf2e58b1c66d8ee0a56e1b89788bc1303799514
HamsterStudent <hwanyamyu@gmail.com>
2023-02-07 18:38:22 +0900

    form event for typescript

--- challenge/typescript_new/README.md
+++ challenge/typescript_new/README.md
@@ -1,1 +1,3 @@
 # typescript
+
+- 3.0 to 3.7 : Typescript
--- challenge/typescript_new/src/App.tsx
+++ challenge/typescript_new/src/App.tsx
@@ -1,10 +1,30 @@
-import Circle from "./Circle";
+import { useState } from "react";
 
 function App() {
+  const [value, setValue] = useState("");
+  // 이벤트에도 타입 추가 이벤트 어케암? 검색....
+  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+    // currentTarget는 target과 같은데 typescript에서는 전자를 더 많이 씀
+    const {
+      currentTarget: { value },
+    } = event;
+    setValue(value);
+  };
+  const onSubmit = (event: React.FocusEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log("hello", value);
+  };
   return (
     <div>
-      <Circle bgColor="teal" borderColor="pink" />
-      <Circle bgColor="pink" text="hamster" />
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={value}
+          type="text"
+          placeholder="username"
+        />
+        <button>Log in</button>
+      </form>
     </div>
   );
 }
