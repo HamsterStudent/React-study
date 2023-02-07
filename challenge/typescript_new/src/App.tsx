91be9c18341f939868bac4523adf44fcce4aec2d
HamsterStudent <hwanyamyu@gmail.com>
2023-02-07 18:11:17 +0900

    typescript-optional props & state

---
+++ challenge/typescript_new/src/App.tsx
@@ -0,0 +1,12 @@
+import Circle from "./Circle";
+
+function App() {
+  return (
+    <div>
+      <Circle bgColor="teal" borderColor="pink" />
+      <Circle bgColor="pink" text="hamster" />
+    </div>
+  );
+}
+
+export default App;
